174d8115e29e809819c0683f551e94f68c2f4ea2
Sergio Manuel Aravena Guerra <sergio.guetta17@gmail.com>
2017-04-04 17:50:06 -0300

    Se agrego metodo actualizar de medicamento

--- AutomatizacionLibretaMedicamento/src/java/dao/MedicamentoDao.java
+++ AutomatizacionLibretaMedicamento/src/java/dao/MedicamentoDao.java
@@ -71,6 +71,43 @@ public class MedicamentoDao {
         
     }
     
+    public boolean modificar(MedicamentoDto dto){
+        try {
+        Connection conexion = Conexion.getConexion();
+        String query = "UPDATE Medicamento SET Nombre = ? ,Tipo = ? , "
+                + "Fabricante  = ? , Componentes = ? , Contenido = ? , Cantidad = ? , "
+                + "Gramaje = ? , Fecha_Vencimiento = ? , Estado = ? , Id_seccion = ? "
+                + " WHERE Codigo = ? ";
+        PreparedStatement modificar = conexion.prepareStatement(query);
+        
+        
+        modificar.setString(1, dto.getNombre());
+            modificar.setString(2, dto.getTipo());
+            modificar.setString(3, dto.getFabricante());
+            modificar.setString(4, dto.getComponente());
+            modificar.setString(5, dto.getContenido());
+            modificar.setInt(6, dto.getCantidad());
+            modificar.setInt(7, dto.getGramaje());
+            modificar.setDate(8,
+                    new java.sql.Date(dto.getFecha_vencimiento().getTime()));
+            modificar.setInt(9, dto.getCodigo());
+
+            modificar.executeUpdate();
+            
+            modificar.close();
+            conexion.close();
+            
+             return true ;    
+        }catch(SQLException w) { 
+            System.out.println("Error al modifcar el medicamento "+w.getMessage());
+            return false;
+        } catch (Exception e) {
+            System.out.println("Error al modificar "+e.getMessage());
+            return false;
+        }
+    }
+    
+    
     public List<MedicamentoDto> listarMedicamentos(){
         List<MedicamentoDto> lista = null;
         try {
