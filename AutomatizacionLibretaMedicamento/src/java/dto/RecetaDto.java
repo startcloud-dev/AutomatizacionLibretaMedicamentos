1a12b9a8e2dbae17a8511c73cbc1f9c0fbad93b5
sergio aravena <sergio.guetta17@gmail.com>
2017-05-10 00:33:19 -0300

    Se agregar Caso 7 De receta

---
+++ AutomatizacionLibretaMedicamento/src/java/dto/RecetaDto.java
@@ -0,0 +1,49 @@
+
+package dto;
+
+import java.util.Date;
+
+/**
+ *
+ * @author LeslieK
+ */
+public class RecetaDto {
+    private int id_receta;
+    private Date fecha_emision;
+    private String indicaciones;
+
+    public RecetaDto() {
+    }
+
+    public int getId_receta() {
+        return id_receta;
+    }
+
+    public void setId_receta(int id_receta) {
+        this.id_receta = id_receta;
+    }
+
+    public Date getFecha_emision() {
+        return fecha_emision;
+    }
+
+    public void setFecha_emision(Date fecha_emision) {
+        this.fecha_emision = fecha_emision;
+    }
+
+    public String getIndicaciones() {
+        return indicaciones;
+    }
+
+    public void setIndicaciones(String indicaciones) {
+        this.indicaciones = indicaciones;
+    }
+
+    @Override
+    public String toString() {
+        return "RecetaDto{" + "id_receta=" + id_receta + ", fecha_emision=" + fecha_emision + ", indicaciones=" + indicaciones + '}';
+    }
+    
+    
+    
+}
---
+++ AutomatizacionLibretaMedicamento/src/java/servlet/ModificarReceta.java
@@ -0,0 +1,84 @@
+
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import dto.RecetaDto;
+import java.sql.Date;
+/**
+ *
+ * @author Sergio
+ */
+public class ModificarReceta extends HttpServlet {
+
+   
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            
+             RecetaDto dto = new RecetaDto();
+            dto.setId_receta(Integer.valueOf(request.getParameter("txtReceta".trim())));
+            dto.setFecha_emision(Date.valueOf(request.getParameter("txtFechaEmi")));
+            dto.setIndicaciones(request.getParameter("txtIndicaciones".trim()));
+
+            String mensaje="";
+            if (new dao.RecetaDaoImp().modificar(dto)) {
+
+                mensaje = "Receta modificada";
+            } else {
+
+                mensaje = "Receta no modificada";
+            }
+            request.setAttribute("mensaje", mensaje);
+            request.setAttribute("lista", new dao.RecetaDaoImp().listar());
+
+            request.getRequestDispatcher("Doctor/ModificarReceta.jsp").forward(request, response);
+            
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
